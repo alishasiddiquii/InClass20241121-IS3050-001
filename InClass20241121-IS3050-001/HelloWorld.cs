e0841c6e79682df65e01affb589d04da59c6e53c
Cole Crooks <crookscl@mail.uc.edu>
2024-11-21 11:38:11 -0500

    Step 10

--- InClass20241121-IS3050-001/HelloWorld.cs
+++ InClass20241121-IS3050-001/HelloWorld.cs
@@ -7,7 +7,14 @@ namespace InClass20241121_IS3050_001
 {
     public class HelloWorld
     {
-        public static void
-            Console.Writeline("Hey!");
+       public static void Welcome()
+        { 
+        Console.WriteLine("Hey!");
     }
+
+    public static void WelcomeResponse()
+    {
+        Console.WriteLine("Hey Bestie!");
+    }
+}
 }
