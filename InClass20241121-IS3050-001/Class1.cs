cb980c1a2d1d91c537496caa56cb6fea5edae36b
siddiqas <siddiqas@LCBLAB2110-38>
2024-11-21 11:49:12 -0500

    osu will crush IU

---
+++ InClass20241121-IS3050-001/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InClass20241121_IS3050_001
+{
+    public class Class1
+    {
+        public class Hello()
+            Public Static Void Hello()
+        {
+            Console.WriteLine("OSU will crush IU");
+        }
+            }
+}
